36622aa890a7eb07d05d6ca47bc27e087ece27e0
João Pedro Nogueira Lucas <joaonl577@gmail.com>
2023-10-03 14:22:35 -0300

    Criando a classe D

---
+++ classD.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class D {
+private:
+	int D1;
+	float D2;
+
+public:
+	D() {
+		D1 = 0;
+		D2 = 0;
+	}
+
+	int get_D1() {
+		return D1;
+	}
+
+	void set_D1(int value) {
+		D1 = value;
+	}
+
+	float get_D2() {
+		return D2;
+	}
+
+	void set_D2(float value) {
+		D2 = value;
+	}
+
+	void MD1() {
+		cout << "Método MD1 da classe D" << endl;
+	}
+
+	void MD2() {
+		cout << "Método MD2 da classe D" << endl;
+	}
+};
